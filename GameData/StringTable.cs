0a9c4b100694ef185f48cefc5ecd93b6209714c3
Markus Heikkinen <ittevien@gmail.com>
2020-07-28 23:01:40 +0300

    Add xml string table support

--- GameData/StringTable.cs
+++ GameData/StringTable.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
+using System.Xml;
 
 namespace Tatti3.GameData
 {
@@ -32,7 +33,55 @@ namespace Tatti3.GameData
 
         public static StringTable FromXml(Stream input)
         {
-            throw new NotImplementedException();
+            var convertedInput = PreprocessXml(input);
+            var self = new StringTable();
+            var xml = new XmlDocument();
+            xml.LoadXml(convertedInput);
+            self.byIndex.Add("(None)");
+            foreach (var val in xml["strings"])
+            {
+                if (val is XmlElement xmlString)
+                {
+                    var key = xmlString["id"].InnerText;
+                    var value = xmlString["value"].InnerText;
+                    self.keyToIndex[key] = (uint)self.byIndex.Count;
+                    self.byIndex.Add(value);
+                }
+            }
+            return self;
+        }
+
+        // .NET doesn't seem to support raw control codes in xml, so convert them to &#N;
+        static string PreprocessXml(Stream input)
+        {
+            string result;
+            using (var reader = new StreamReader(input))
+            {
+                result = reader.ReadToEnd();
+            }
+            var builder = new StringBuilder(result.Length);
+            var pos = 0;
+            while (true)
+            {
+                var substringStart = pos;
+                while (pos != result.Length)
+                {
+                    var ch = result[pos];
+                    if (ch < 0x20 && ch != '\r' && ch != '\n')
+                    {
+                        break;
+                    }
+                    pos += 1;
+                }
+                builder.Append(result, substringStart, pos - substringStart);
+                if (pos == result.Length)
+                {
+                    break;
+                }
+                builder.Append($"&#{(int)result[pos]};");
+                pos += 1;
+            }
+            return builder.ToString();
         }
 
         public string? GetByIndex(uint index)
